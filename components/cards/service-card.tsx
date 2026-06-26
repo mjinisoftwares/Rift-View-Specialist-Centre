e30ffd820f3625ed520522a9c52d45bff30e7fca
mjini softwares <njauvictor.maina@gmail.com>
2026-06-26 08:56:09 +0300

    add service  commit

--- components/cards/service-card.tsx
+++ components/cards/service-card.tsx
@@ -114,6 +114,7 @@ export function ServiceCard({ service, index = 0 }: ServiceCardProps) {
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.95 }}
             aria-hidden="true"
+            tabIndex={-1}
           >
             <ArrowRight className="h-4 w-4 transition-transform duration-300 group-hover:translate-x-0.5" />
           </motion.span>
